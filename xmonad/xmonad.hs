cb7ffca502968fcd63d7822c3e4645548d5e2c89
Florian Engel <floriaengel39@gmail.com>
2018-09-25 15:22:42 +0200

    Xmonad: Remove trailing spaces

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -35,7 +35,7 @@ data LibNotifyUrgencyHook = LibNotifyUrgencyHook deriving (Read, Show)
 newtype ListStorage = ListStorage [WorkspaceId] deriving Typeable
 
 instance ExtensionClass ListStorage where
-  initialValue = ListStorage ["1"]        
+  initialValue = ListStorage ["1"]
 
 main :: IO ()
 main = xmonad
@@ -48,8 +48,8 @@ main = xmonad
      , manageHook      = myManageHooks
      , startupHook     = setWMName "LG3D"
      , handleEventHook = handleEventHook def <+> fullscreenEventHook
-     } 
-     `additionalKeys` 
+     }
+     `additionalKeys`
      ([ ((0, 0x1008ff13), spawn "~/scripts/volumeplus")
      , ((0, 0x1008ff11),  spawn "~/scripts/volumeminus")
      , ((0, 0x1008ff12),  spawn "~/scripts/mute")
@@ -61,20 +61,20 @@ main = xmonad
      , ((mod4Mask, xK_v), spawn "emacsclient -c init.org")
      , ((mod4Mask .|. shiftMask, xK_v), composeAll [viewEmptyWorkspace, spawn "emacsclient -c init.org",saveFocus])
      , ((mod4Mask, xK_f), composeAll [actionMenu, saveFocus])
-     , ((mod4Mask, xK_g), goToNotify ) 
-     , ((mod4Mask, xK_j), GH.focusUp) 
-     , ((mod4Mask, xK_k), GH.focusDown) 
-     , ((mod4Mask, xK_h), shrinkMasterGroups) 
-     , ((mod4Mask, xK_l), expandMasterGroups) 
-     , ((mod4Mask .|. shiftMask, xK_j), GH.swapUp) 
-     , ((mod4Mask .|. shiftMask, xK_k), GH.swapDown) 
-     , ((mod4Mask, xK_s), GH.focusGroupMaster) 
-     , ((mod4Mask, xK_a), GH.focusGroupUp) 
-     , ((mod4Mask, xK_d), GH.focusGroupDown) 
-     , ((mod4Mask .|. shiftMask, xK_a), GH.moveToGroupUp False) 
-     , ((mod4Mask .|. shiftMask, xK_d), GH.moveToGroupDown False) 
+     , ((mod4Mask, xK_g), goToNotify )
+     , ((mod4Mask, xK_j), GH.focusUp)
+     , ((mod4Mask, xK_k), GH.focusDown)
+     , ((mod4Mask, xK_h), shrinkMasterGroups)
+     , ((mod4Mask, xK_l), expandMasterGroups)
+     , ((mod4Mask .|. shiftMask, xK_j), GH.swapUp)
+     , ((mod4Mask .|. shiftMask, xK_k), GH.swapDown)
+     , ((mod4Mask, xK_s), GH.focusGroupMaster)
+     , ((mod4Mask, xK_a), GH.focusGroupUp)
+     , ((mod4Mask, xK_d), GH.focusGroupDown)
+     , ((mod4Mask .|. shiftMask, xK_a), GH.moveToGroupUp False)
+     , ((mod4Mask .|. shiftMask, xK_d), GH.moveToGroupDown False)
  ]
-     ++ [((mod4Mask, k), composeAll 
+     ++ [((mod4Mask, k), composeAll
         [windows .  W.greedyView $ show i, saveFocus])
         | (i, k) <- zip [1 ..9] [xK_1 .. xK_9]]
     )
@@ -85,10 +85,10 @@ main = xmonad
       where
               listWindows= "termite -e ~/projects/python/listWindows/listWindows.py"
               dbus = "dbus-send --print-reply "
-              dest = "--dest=org.mpris.MediaPlayer2.spotify " 
+              dest = "--dest=org.mpris.MediaPlayer2.spotify "
               org = "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player."
 
-    
+
 
 -- Dont work use instead m,n
 -- 0x1008ff02 = XFMonBrightnessUp
@@ -101,7 +101,7 @@ shrinkMasterGroups = sendMessage $ G.ToEnclosing $ SomeMessage Shrink
 -- | Expand the master area
 expandMasterGroups :: X ()
 expandMasterGroups = sendMessage $ G.ToEnclosing $ SomeMessage Expand
- 
+
 myManageHooks = composeAll
     [ isFullscreen --> doFullFloat
     , manageSpawn <+> manageHook def
@@ -109,18 +109,18 @@ myManageHooks = composeAll
     , className =? "feh" --> doFullFloat
     ]
 
-myLayout = Full ||| shrinked ||| tapped 
-  where shrinked = gaps [(L,300), (R,300)] Full 
-        tapped   = G.group T.simpleTabbed $ Mirror $ Column 1 
+myLayout = Full ||| shrinked ||| tapped
+  where shrinked = gaps [(L,300), (R,300)] Full
+        tapped   = G.group T.simpleTabbed $ Mirror $ Column 1
 
 -- | programms that can be start from dmenu
 programms :: [(String,String)] -- ^ (name in dmenu, executable)
-programms = [ ("firefox","firefox") 
+programms = [ ("firefox","firefox")
             , ("anki", "anki")
             , ("signal", "signal-desktop")
             , ("spotify", "spotify")
-            , ("netflix", "google-chrome-stable \"netflix.com\"") 
-            , ("mutt", startTerm "mutt") 
+            , ("netflix", "google-chrome-stable \"netflix.com\"")
+            , ("mutt", startTerm "mutt")
             , ("calcurse", startTerm "calcurse")
             , ("toxic", startTerm "toxic")
             , ("rtv", startTerm "rtv")
@@ -129,34 +129,34 @@ programms = [ ("firefox","firefox")
             , ("termite", "termite")
             ]
   where
-    startTerm s = "termite --title=" ++ s ++ " --exec=" ++ s 
+    startTerm s = "termite --title=" ++ s ++ " --exec=" ++ s
 
 -- | save the current workspace, so you can later come back with goToNotify
-saveFocus :: X() 
-saveFocus = do 
+saveFocus :: X()
+saveFocus = do
    i <- (W.tag . W.workspace . W.current) <$> gets windowset
-   XS.modify $ changeHead i 
+   XS.modify $ changeHead i
 
-changeHead :: WorkspaceId -> ListStorage -> ListStorage 
-changeHead _ l@(ListStorage [])   = l 
+changeHead :: WorkspaceId -> ListStorage -> ListStorage
+changeHead _ l@(ListStorage [])   = l
 changeHead i (ListStorage (_:xs)) = ListStorage $ i:xs
 
 -- | cycle through the windows that triggered urgency and go back to the window,
 --   you came from
 goToNotify :: X()
-goToNotify =  do 
+goToNotify =  do
        i <- lastStorage <$> XS.get
        XS.modify deleteN
-       windows $ W.greedyView i 
+       windows $ W.greedyView i
 
 lastStorage :: ListStorage -> WorkspaceId
 lastStorage (ListStorage xs) = last xs
 
 deleteN :: ListStorage -> ListStorage
-deleteN l@(ListStorage [x]) = l 
+deleteN l@(ListStorage [x]) = l
 deleteN (ListStorage xs)    = ListStorage $ init xs
 
--- | if a window triggers a urgency, save the workspace on which it is 
+-- | if a window triggers a urgency, save the workspace on which it is
 --   and show a notification
 instance UrgencyHook LibNotifyUrgencyHook where
     urgencyHook LibNotifyUrgencyHook w = do
@@ -164,23 +164,23 @@ instance UrgencyHook LibNotifyUrgencyHook where
         unless (match $ show name) $ do
           Just idx <- W.findTag w <$> gets windowset
           safeSpawn "notify-send" ["-t 3000", show name, "workspace " ++ idx]
-          XS.modify $ addCont idx 
-        where 
+          XS.modify $ addCont idx
+        where
           unwantedNotifcations = [ "qutebrowser" ]
           match n = any (matchEnd n) unwantedNotifcations
-          matchEnd n uN = take (length uN) (reverse n) == reverse uN 
-    
+          matchEnd n uN = take (length uN) (reverse n) == reverse uN
 
--- | add the workspace at the end of the list, if it isn't already in it 
-addCont :: WorkspaceId -> ListStorage -> ListStorage 
+
+-- | add the workspace at the end of the list, if it isn't already in it
+addCont :: WorkspaceId -> ListStorage -> ListStorage
 addCont i l@(ListStorage xs) | i `elem` xs = l
-                             | otherwise   = ListStorage $ xs ++ [i]  
+                             | otherwise   = ListStorage $ xs ++ [i]
 
 
 -- | Calls dmenuMap to grab the appropriate Window, and hands it off to action
 --   if found.
 actionMenu :: X ()
-actionMenu = windowMap >>= menuMapFunction >>= flip whenJust id 
+actionMenu = windowMap >>= menuMapFunction >>= flip whenJust id
     where
       menuMapFunction :: M.Map String a -> X (Maybe a)
       menuMapFunction = menuMapArgs "dmenu" ["-i","-l","30"]
@@ -189,16 +189,16 @@ actionMenu = windowMap >>= menuMapFunction >>= flip whenJust id
 windowMap :: X (M.Map String (X ()))
 windowMap = do
   ws <- gets windowset
-  M.fromList . (++ openProgramms) . toAction . concat 
+  M.fromList . (++ openProgramms) . toAction . concat
     <$> mapM keyValuePairs (W.workspaces ws)
  where keyValuePairs ws = mapM (keyValuePair ws) $ W.integrate' (W.stack ws)
        keyValuePair ws w = flip (,) w <$> decorateName ws w
-       toAction = map (second (windows . W.focusWindow)) 
-       openProgramms = map 
-                         (\(name,prog) 
+       toAction = map (second (windows . W.focusWindow))
+       openProgramms = map
+                         (\(name,prog)
                            -> ("~ " ++ name ++ " ~",
                               composeAll [viewEmptyWorkspace,
-                                           spawn prog,saveFocus])) 
+                                           spawn prog,saveFocus]))
                          programms
 
 
