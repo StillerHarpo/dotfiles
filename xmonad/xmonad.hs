36586e016574399885e8b59910fbb144fd6bb23f
Florian Engel <florianengel39@gmail.com>
2019-09-27 18:21:08 +0200

    Make run or raise window better

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,37 +1,43 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE TupleSections #-}
-import XMonad
-import Control.Arrow(second)
-import Control.Monad(unless)
-import Control.Concurrent
-import qualified XMonad.StackSet as W
+{-# LANGUAGE RankNTypes #-}
+
+import           Control.Applicative
+import           Control.Arrow (second)
+import           Control.Concurrent
+import           Control.Monad (unless, void)
+import           Data.Default
+import           Data.List
 import qualified Data.Map as M
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.UrgencyHook
-import XMonad.Util.NamedWindows
-import XMonad.Util.Run
-import XMonad.Util.EZConfig
-import XMonad.Layout.NoBorders
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Actions.SpawnOn
-import System.Process
-import System.IO
-import System.Directory
-import XMonad.Actions.FindEmptyWorkspace
-import XMonad.Actions.PhysicalScreens
-import XMonad.Hooks.SetWMName
-import Control.Applicative
-import Data.Default
-import Data.List
-import XMonad.Layout.Gaps
-import XMonad.Util.Dzen
-import XMonad.Util.Dmenu(menuMapArgs)
-import qualified XMonad.Layout.Tabbed as T
+import           System.Directory
+import           System.IO
+import           System.Process
+import           XMonad
+import           XMonad.Actions.CycleWS
+import           XMonad.Actions.FindEmptyWorkspace
+import           XMonad.Actions.PhysicalScreens
+import           XMonad.Actions.SpawnOn
+import           XMonad.Hooks.EwmhDesktops
+import           XMonad.Hooks.ManageHelpers
+import           XMonad.Hooks.SetWMName
+import           XMonad.Hooks.UrgencyHook
+import           XMonad.Layout.Column
+import           XMonad.Layout.Gaps
 import qualified XMonad.Layout.Groups as G
 import qualified XMonad.Layout.Groups.Helpers as GH
-import XMonad.Layout.Column
+import           XMonad.Layout.NoBorders
+import qualified XMonad.Layout.Tabbed as T
+import           XMonad.Prompt hiding (font)
+import           XMonad.Prompt.RunOrRaise
+import qualified XMonad.StackSet as W
+import           XMonad.Util.Dmenu (menuMapArgs)
+import           XMonad.Util.Dzen
+import           XMonad.Util.EZConfig
 import qualified XMonad.Util.ExtensibleState as XS
-import XMonad.Actions.CycleWS
+import           XMonad.Util.NamedWindows
+import           XMonad.Util.Run
+import           XMonad.Util.Stack
+import           XMonad.Actions.MessageFeedback
 
 data LibNotifyUrgencyHook = LibNotifyUrgencyHook deriving (Read, Show)
 
@@ -65,7 +71,9 @@ main = xmonad
       , ((mod4Mask .|. shiftMask, xK_b)      , spawnOnEmpty "qutebrowser")
       , ((mod4Mask              , xK_v)      , spawn        emacs)
       , ((mod4Mask .|. shiftMask, xK_v)      , spawnOnEmpty emacs)
-      , ((mod4Mask              , xK_f)      , composeAll [actionMenu
+      , ((mod4Mask              , xK_f)      , composeAll [runOrShift
+                                                          , saveFocus])
+      , ((mod4Mask .|. shiftMask, xK_f)      , composeAll [runOrRaise
                                                           , saveFocus])
       , ((mod4Mask              , xK_g)      , goToNotify )
       , ((mod4Mask              , xK_j)      , GH.focusUp)
@@ -160,7 +168,7 @@ volume vol = script >> output >>= myDzenConfig 300
       <$> runProcessWithInput "pacmd" ["list-sinks"] ""
 
 backlight :: Direction -> X ()
-backlight dir = runProcessWithInput "light" [ dirOp dir, "10" ] "" >> return ()
+backlight dir = void $ runProcessWithInput "light" [ dirOp dir, "10" ] ""
   where
     dirOp Plus = "-A"
     dirOp Minus = "-U"
@@ -186,7 +194,8 @@ myManageHooks = composeAll
 
 myLayout = Full ||| shrinked ||| tabbed
   where shrinked = gaps [(L,300), (R,300)] Full
-        tabbed   = G.group T.simpleTabbed $ Mirror $ Column 1
+        -- tabbed   = G.group T.simpleTabbed $ Mirror $ Column 1
+        tabbed   = G.group Full $ Mirror $ Column 1
 
 -- | programms that can be start from dmenu
 programms :: [(String,String)] -- ^ (name in dmenu, executable)
@@ -202,6 +211,8 @@ programms = [ ("firefox" ,"firefox")
             , ("newsboat", startTerm "newsboat")
             , ("tor"     , "tor-browser")
             , ("termite" , "termite")
+            , ("VirtualBox" , "VirtualBox")
+            , ("mattermost-desktop" , "mattermost-desktop")
             ]
   where
     startTerm s = "termite --title=" ++ s ++ " --exec=" ++ s
@@ -250,37 +261,78 @@ addCont :: WorkspaceId -> ListStorage -> ListStorage
 addCont i l@(ListStorage xs) | i `elem` xs = l
                              | otherwise   = ListStorage $ xs ++ [i]
 
--- | Calls dmenuMap to grab the appropriate Window, and hands it off to action
---   if found.
-actionMenu :: X ()
-actionMenu = windowMap >>= menuMapFunction >>= flip whenJust id
+-- | Calls dmenuMap to grab the appropriate Window or program, and hands it off to action
+--   if found .
+actionMenu :: (Window -> X()) -- ^ the action
+           -> Bool -- ^ should the program be open on a new workspace
+           -> X ()
+actionMenu action viewEmpty = windowMap >>= menuMapFunction >>= flip whenJust id
     where
       menuMapFunction :: M.Map String a -> X (Maybe a)
-      menuMapFunction = menuMapArgs "dmenu" ["-i","-l","30"]
+      menuMapFunction = menuMapArgs "rofi" ["-dmenu", "-i"]
+      -- menuMapFunction = menuMapArgs "dmenu" ["-i","-l","30"]
 
--- | A map from window names to Windows, given a windowTitler function.
-windowMap :: X (M.Map String (X ()))
-windowMap = do
-  ws <- gets $ W.workspaces . windowset
-  M.fromList . (++ openProgramms) . toAction . concat
-    <$> mapM keyValuePairs ws
- where keyValuePairs ws = mapM (keyValuePair ws) $ W.integrate' (W.stack ws)
-       keyValuePair ws w = (, w) <$> decorateName ws w
-       toAction = map $ second getWindow
-       getWindow w = do
-         Just idx <- W.findTag w <$> gets windowset
-         windows $ W.focusWindow w . W.greedyView idx
-       openProgramms = map
-                         (\(name,prog)
-                           -> ("𞹾" ++ name,
-                              composeAll [viewEmptyWorkspace,
-                                           spawn prog, saveFocus]))
-                         programms
+      -- | A map from window names to Windows, given a windowTitler function.
+      windowMap :: X (M.Map String (X ()))
+      windowMap = do
+        ws <- gets $ W.workspaces . windowset
+        M.fromList . (++ openProgramms) . toAction . concat
+          <$> mapM keyValuePairs ws
+       where keyValuePairs ws = mapM (keyValuePair ws) $ W.integrate' (W.stack ws)
+             keyValuePair ws w = (, w) <$> decorateName ws w
+             toAction = map $ second action
+             openProgramms = map
+                               (\(name,prog)
+                                 -> (name,
+                                    composeAll ([viewEmptyWorkspace | viewEmpty]
+                                                ++ [spawn prog, saveFocus])))
+                               programms
+
+runOrRaise :: X ()
+runOrRaise = actionMenu (windows . W.focusWindow) True
+
+runOrShift :: X()
+runOrShift = actionMenu action False
+  where action w = do
+          idx <- W.currentTag <$> gets windowset
+          windows $ delWin idx w
+          windows $ W.insertUp w
+          insertUp w
 
 -- | Returns the window name as will be listed in dmenu.
 --   Tagged with the workspace ID, to guarantee uniqueness, and to let
---   the user know where he's going.
+--   the user know where they going.
 decorateName :: WindowSpace -> Window -> X String
 decorateName ws w = do
-  name <- show <$> getName w
-  return $ name ++ " [" ++ W.tag ws ++ "]"
+  name <-  resClass <$> withDisplay (\d -> io $ getClassHint d w)
+  displayName <- show <$> getName w
+  return $ name ++ " - " ++ displayName ++ " [" ++ W.tag ws ++ "]"
+
+
+-- | temporally deletes a window on a workspace
+delWin :: (Ord a, Eq s, Eq i) => i -> a -> W.StackSet i l a s sd -> W.StackSet i l a s sd
+delWin n w s = case W.findTag w s of
+                    Just from | n `W.tagMember` s -> go from s
+                    _                             -> s
+    where go from = onWorkspace from (W.delete' w)
+
+-- onZipper, onLayout, addWindows
+
+-- | is not exported from Stackset
+onWorkspace :: (Eq i, Eq s) => i -> (W.StackSet i l a s sd -> W.StackSet i l a s sd)
+            -> (W.StackSet i l a s sd -> W.StackSet i l a s sd)
+onWorkspace n f s = W.view (W.currentTag s) . f . W.view n $ s
+
+-- | inserts a window over the currently focused one
+insertUp w = alt (onFocused $ insertUpZ w) (W.insertUp w)
+
+-- | Group helpers
+onFocused :: (Zipper Window -> Zipper Window) -> G.ModifySpec
+onFocused f _ gs = onFocusedZ (G.onZipper f) gs
+
+alt :: G.ModifySpec -> (WindowSet -> WindowSet) -> X ()
+alt f g = alt2 (G.Modify f) $ windows g
+
+alt2 :: G.GroupsMessage -> X () -> X ()
+alt2 m x = do b <- send m
+              unless b x
